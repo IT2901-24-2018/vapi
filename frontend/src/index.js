4cd150ea3f6385a3eab1ac370d42671cfb136e87
sardred <andelar96@gmail.com>
2018-02-19 18:03:32 +0100

    Added material-ui, with a toggle switch to prove it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Toggle from 'material-ui/Toggle';
+
 import './base.css';
 import './index.css';
 
@@ -11,14 +14,19 @@ import MyMapComponent from './components/MyMapComponent/MyMapComponent';
 import registerServiceWorker from './registerServiceWorker';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to ORM</h1>
-    </header>
-    <p className="App-intro"> {" It's got React, and like, Django, and stuff. "}</p>
-    <MyMapComponent isMarkerShown />
-  </div>
+  <MuiThemeProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to ORM</h1>
+      </header>
+      <Toggle
+        defaultToggled
+      />
+      <p className="App-intro"> {" It's got React, and like, Django, and stuff. "}</p>
+      <MyMapComponent isMarkerShown />
+    </div>
+  </MuiThemeProvider>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
