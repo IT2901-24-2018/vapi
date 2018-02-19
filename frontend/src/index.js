4d3659cf74da646741a8921a3d34af28688b2c6f
sardred <andelar96@gmail.com>
2018-02-19 21:04:45 +0100

    Removed create-react-app filler

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Toggle from 'material-ui/Toggle';
 
 import './base.css';
-import './index.css';
-
-import logo from './assets/logo.svg';
 
 import MyMapComponent from './components/MyMapComponent/MyMapComponent';
 
@@ -16,14 +13,9 @@ import registerServiceWorker from './registerServiceWorker';
 const App = () => (
   <MuiThemeProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to ORM</h1>
-      </header>
       <Toggle
         defaultToggled
       />
-      <p className="App-intro"> {" It's got React, and like, Django, and stuff. "}</p>
       <MyMapComponent isMarkerShown />
     </div>
   </MuiThemeProvider>
