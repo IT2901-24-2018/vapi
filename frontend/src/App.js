06d4f51668efb020b07544458b09786b6a91b9a0
sardred <andelar96@gmail.com>
2018-02-19 17:42:15 +0100

    Renamed media to assets, modified gitignore to support it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './media/logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 import MyMapComponent from './components/MyMapComponent/MyMapComponent';
 
