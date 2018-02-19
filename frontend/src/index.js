c2f297901f3ab2587837f7916e344c9264642b73
sardred <andelar96@gmail.com>
2018-02-19 17:56:04 +0100

    Merged App and index files, removed render test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './base.css';
 import './index.css';
-import App from './App';
+
+import logo from './assets/logo.svg';
+
+import MyMapComponent from './components/MyMapComponent/MyMapComponent';
+
 import registerServiceWorker from './registerServiceWorker';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to ORM</h1>
+    </header>
+    <p className="App-intro"> {" It's got React, and like, Django, and stuff. "}</p>
+    <MyMapComponent isMarkerShown />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
