07b039bf923c911240936f66bde8bb4fe90cb50b
sardred <andelar96@gmail.com>
2018-02-21 17:52:46 +0100

    Added correct SVV color scheme with approximate mapping to MUI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import AppBar from 'material-ui/AppBar';
 
 import './sanitize.css';
@@ -11,8 +13,26 @@ import Map from './components/Map/Map';
 
 import registerServiceWorker from './registerServiceWorker';
 
+const COLOR_SCHEME = {
+  orange: '#ff9600',
+  grayDark: '#444f55',
+  grayMid: '#dadada',
+  blue: '#008ec2',
+  green: '#5db82e',
+  grayLight: '#f5f5f5',
+};
+
+const THEME = getMuiTheme({
+  palette: {
+    primary1Color: COLOR_SCHEME.grayDark,
+    primary2Color: COLOR_SCHEME.orange,
+    accent1Color: COLOR_SCHEME.orange,
+    pickerHeaderColor: COLOR_SCHEME.grayDark,
+  },
+});
+
 const App = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={THEME}>
     <AppBar
       title="Orm"
       showMenuIconButton={false}
