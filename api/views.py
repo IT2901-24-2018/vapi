7929c4f8af09be95f159dc56d3a251c728d3e9e8
Pontius1007 <embrikeinang@mns.com>
2018-02-28 16:34:57 +0100

    Remove API root

--- api/views.py
+++ api/views.py
@@ -1,19 +1,8 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import viewsets, permissions
 from api.models import RoadSegment
 from api.serializers import RoadSegmentSerializer, UserSerializer
 from django.contrib.auth.models import User
 from api.permissions import IsAdminOrReadOnly
-
-
-@api_view
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'roadsegments': reverse('roadsegment-list', request=request, format=format)
-    })
 
 
 class RoadSegmentViewSet(viewsets.ModelViewSet):
