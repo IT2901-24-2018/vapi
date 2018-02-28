eb87287c875c6c08611c80f10a2dc4f397cdf457
Sebastian Olafsson <msemse.sebastian@gmail.com>
2018-02-28 15:47:33 +0100

    added SVV maps and fixed projections to work with normal coordinates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import 'leaflet/dist/leaflet.css';
+
 
 import { Map, TileLayer } from 'react-leaflet';
 
@@ -17,6 +19,7 @@ import Toggle from 'material-ui/Toggle';
 import DatePicker from 'material-ui/DatePicker';
 import * as _colorManipulator from 'material-ui/utils/colorManipulator';
 
+import 'proj4leaflet';
 import './sanitize.css';
 import './base.css';
 
@@ -53,10 +56,39 @@ const THEME = getMuiTheme({
   },
 });
 
-const stamenTonerTiles = 'http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png';
-const stamenTonerAttr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-const mapCenter = [39.9528, -75.1638];
-const zoomLevel = 12;
+const mapCenter = [63.417400, 10.397518];
+const zoomLevel = 14;
+
+const crs = new window.L.Proj.CRS(
+  'EPSG:25833',
+  '+proj=utm +zone=33 +ellps=GRS80 +units=m +no_defs ',
+  {
+    resolutions: [
+      21674.7100160867,
+      10837.35500804335,
+      5418.677504021675,
+      2709.3387520108377,
+      1354.6693760054188,
+      677.3346880027094,
+      338.6673440013547,
+      169.33367200067735,
+      84.66683600033868,
+      42.33341800016934,
+      21.16670900008467,
+      10.583354500042335,
+      5.291677250021167,
+      2.6458386250105836,
+      1.3229193125052918,
+      0.6614596562526459,
+      0.33072982812632296,
+      0.16536491406316148,
+    ],
+    origin: [-2500000.0, 9045984.0],
+    // transformation: Leaflet.Transformation(1, 0, -1, 0)
+  },
+
+);
+
 
 const App = () => (
   <MuiThemeProvider muiTheme={THEME}>
@@ -91,10 +123,10 @@ const App = () => (
       <Map
         center={mapCenter}
         zoom={zoomLevel}
+        crs={crs}
       >
         <TileLayer
-          attribution={stamenTonerAttr}
-          url={stamenTonerTiles}
+          url="http://nvdbcache.geodataonline.no/arcgis/rest/services/Trafikkportalen/GeocacheTrafikkJPG/MapServer/tile/{z}/{y}/{x}"
         />
       </Map>
     </div>
