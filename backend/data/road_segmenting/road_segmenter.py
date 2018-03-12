571b0506f4eaee25a64e5b92d494dd86bc87145f
Pontius1007 <embrikeinang@msn.com>
2018-03-12 12:20:08 +0100

    Initial file setup

---
+++ backend/data/road_segmenting/road_segmenter.py
@@ -0,0 +1,12 @@
+
+
+def distance_between_utm(list_start, list_end):
+    # Calculates the distance between two UTM coordinates so we can properly split it
+
+
+def split_segment(veg):
+    # Main logic
+
+
+def check_split(veg):
+    # check if road segment is too long
