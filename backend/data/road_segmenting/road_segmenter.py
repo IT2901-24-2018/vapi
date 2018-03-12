2c7b13ecb306798270907700998e295df8767952
Pontius1007 <embrikeinang@msn.com>
2018-03-12 17:04:18 +0100

    Can access coordinates

--- backend/data/road_segmenting/road_segmenter.py
+++ backend/data/road_segmenting/road_segmenter.py
@@ -1,12 +1,28 @@
+from road_fetcher import *
+
+def split_segment(road_network):
+    # Main logic
+    return None
 
 
 def distance_between_utm(list_start, list_end):
     # Calculates the distance between two UTM coordinates so we can properly split it
+    #x1, x2
+    #y1, y2
+    #distance = sqrt((x1-x2)² + (y1-y2)²)
+    #do we need to take height into the calculation?
+    return None
 
 
-def split_segment(veg):
-    # Main logic
-
-
-def check_split(veg):
+def check_split(road, meter):
     # check if road segment is too long
+    return None
+
+
+def main(kommune, vegref):
+    results = vegnet_to_geojson(kommune, vegref)
+    count, road_net = results[0], results[1]
+    for key, values in road_net.items():
+        if(key != 'crs'):
+            for x in range(0, count):
+                print(road_net['features'][x]['geometry']['coordinates'][0])
