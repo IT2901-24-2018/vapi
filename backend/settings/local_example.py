68c13cca919d7cc9c5f6aaec19154df4c66af56e
Håvard Hellem <haavahe@users.noreply.github.com>
2018-03-16 17:13:45 +0100

    Add debug = true to template

--- backend/settings/local_example.py
+++ backend/settings/local_example.py
@@ -1,6 +1,8 @@
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+DEBUG = True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
