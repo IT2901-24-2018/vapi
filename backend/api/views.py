34288d600cd6603451f9e8a5d6630d9f965159dd
Pontius1007 <embrikeinang@msn.com>
2018-04-04 14:38:49 +0200

    View for RoadSemgent done

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, permissions
-from api.models import RoadSegment
-from api.serializers import RoadSegmentSerializer, UserSerializer
 from django.contrib.auth.models import User
-from api.permissions import IsAdminOrReadOnly
+from rest_framework import permissions, status, viewsets
+from rest_framework.response import Response
+
+from api.models import ProductionData, RoadSegment
+from api.permissions import IsAdminOrReadOnly, IsStaffOrCreateOnly
+from api.serializers import ProductionDataSerializer, RoadSegmentSerializer, UserSerializer
 
 
 class RoadSegmentViewSet(viewsets.ModelViewSet):
@@ -13,6 +15,30 @@ class RoadSegmentViewSet(viewsets.ModelViewSet):
     queryset = RoadSegment.objects.all()
     serializer_class = RoadSegmentSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsAdminOrReadOnly,)
+
+    def create(self, request, *args, **kwargs):
+        """
+        Add support for creating when the input data is a list
+        """
+        many = False
+
+        # Check if the incoming data is a list
+        # If it is a list set the many flag to True
+        if isinstance(request.data, list):
+            many = True
+
+        # Instantiate the serializer
+        serializer = self.get_serializer(data=request.data, many=many)
+
+        # Check if the serializer is valid and takes the necessary actions
+        if serializer.is_valid():
+            serializer.save()
+            headers = self.get_success_headers(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED,
+                            headers=headers)
+
+        # If not valid return error
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
