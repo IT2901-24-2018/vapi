f454597b326241d351b5f79c808eb777379526c3
Pontius1007 <embrikeinang@msn.com>
2018-04-06 17:16:47 +0200

    Fix list bug

--- backend/data/road_segmenting/road_segmenter.py
+++ backend/data/road_segmenting/road_segmenter.py
@@ -70,8 +70,11 @@ def road_segmentor(kommune, vegref, max_distance, min_gps):
         if key != 'crs':
             for road_segment in range(0, count):
                 if check_split(road_net['features'][road_segment], max_distance):
-                    segmented_road_network.append(split_segment(road_net['features'][road_segment],
-                                                                max_distance, [], min_gps))
+                    split_roads = split_segment(road_net['features'][road_segment],
+                                                max_distance, [], min_gps)
+                    if split_roads is not None:
+                        for new_road in split_roads:
+                            segmented_road_network.append(new_road)
                 else:
                     segmented_road_network.append(road_net['features'][road_segment])
     return segmented_road_network
