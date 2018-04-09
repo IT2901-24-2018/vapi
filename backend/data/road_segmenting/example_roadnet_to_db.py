ef7b723d70ea5407f6cfa3c6ce83c35a1d8de338
Pontius1007 <embrikeinang@msn.com>
2018-04-09 15:06:19 +0200

    Translated, fixed type and made variables for magic numbers

--- backend/data/road_segmenting/example_roadnet_to_db.py
+++ backend/data/road_segmenting/example_roadnet_to_db.py
@@ -1,6 +1,15 @@
 import requests
 from road_segmenter import road_segmentor
 
+municipality = 5001
+type_of_road = 'kg'
+max_distance = 100
+min_segments = 2
+
+# Credentials for connecting and writing to the API
+API_username = "kuk"
+API_password = "1234qweasd"
+
 
 def filter_road(road):
     """
@@ -12,31 +21,31 @@ def filter_road(road):
 
     filtered_road['coordinates']        = ', '.join(str(item) for innerlist in road['geometry']['coordinates']
                                                     for item in innerlist)
-    filtered_road['fra_meter']          = road['properties']['fra_meter']
-    filtered_road['fylke']              = road['properties']['fylke']
+    filtered_road['from_meter']         = road['properties']['fra_meter']
+    filtered_road['county']             = road['properties']['fylke']
     filtered_road['srid']               = road['properties']['geometri']['srid']
     filtered_road['hp']                 = road['properties']['hp']
     filtered_road['href']               = road['properties']['href']
-    filtered_road['kategori']           = road['properties']['kategori']
-    filtered_road['kommune']            = road['properties']['kommune']
-    filtered_road['konnekteringslenke'] = road['properties']['konnekteringslenke']
-    filtered_road['kortform']           = road['properties']['kortform']
+    filtered_road['category']           = road['properties']['kategori']
+    filtered_road['municipality']       = road['properties']['kommune']
+    filtered_road['connlink']           = road['properties']['konnekteringslenke']
+    filtered_road['shortform']          = road['properties']['kortform']
     filtered_road['medium']             = road['properties']['medium']
-    filtered_road['startdato']          = road['properties']['metadata']['startdato']
-    filtered_road['nummer']             = road['properties']['nummer']
+    filtered_road['startdate']          = road['properties']['metadata']['startdato']
+    filtered_road['number']             = road['properties']['nummer']
     filtered_road['region']             = road['properties']['region']
-    filtered_road['sluttnode']          = road['properties']['sluttnode']
-    filtered_road['sluttposisjon']      = road['properties']['sluttposisjon']
+    filtered_road['endnode']            = road['properties']['sluttnode']
+    filtered_road['endposition']        = road['properties']['sluttposisjon']
     filtered_road['startnode']          = road['properties']['startnode']
-    filtered_road['startposisjon']      = road['properties']['startposisjon']
+    filtered_road['startposition']      = road['properties']['startposisjon']
     filtered_road['status']             = road['properties']['status']
-    filtered_road['strekningslengde']   = road['properties']['strekningslengde']
-    filtered_road['temakode']           = road['properties']['temakode']
-    filtered_road['til_meter']          = road['properties']['til_meter']
-    filtered_road['typeveg']            = road['properties']['typeVeg']
-    filtered_road['vegavdeling']        = road['properties']['vegavdeling']
-    filtered_road['veglenkeid']         = road['properties']['veglenkeid']
-    filtered_road['vrefkortform']       = road['properties']['vrefkortform']
+    filtered_road['stretchdistance']    = road['properties']['strekningslengde']
+    filtered_road['themecode']          = road['properties']['temakode']
+    filtered_road['to_meter']           = road['properties']['til_meter']
+    filtered_road['typeofroad']         = road['properties']['typeVeg']
+    filtered_road['roadsection']        = road['properties']['vegavdeling']
+    filtered_road['roadsectionid']      = road['properties']['veglenkeid']
+    filtered_road['vrefshortform']      = road['properties']['vrefkortform']
 
     return filtered_road
 
@@ -61,9 +70,10 @@ def data_in(kommune, type_road, max_distance, min_segments):
     url = 'http://localhost:8000/api/roadsegments/'
     filtered_network = format_to_db(kommune, type_road, max_distance, min_segments)
 
-    r = requests.post(url, json=filtered_network, auth=('YOU_DB_USERNAME', 'YOUR_DB_PASSWORD'))
+    r = requests.post(url, json=filtered_network, auth=(API_username, API_password))
     return "Status: {}\n{}".format(r.status_code, r.text)
 
 
 if __name__ == '__main__':
-    data_in(5001, 'kg', 100, 2)
+    print(data_in(municipality, type_of_road, max_distance, min_segments))
+
