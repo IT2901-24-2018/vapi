d3eb3881871b2a47df5ddd284b25635997846e77
Pontius1007 <embrikeinang@msn.com>
2018-04-11 11:06:55 +0200

    Fixed typo

--- backend/data/road_segmenting/test_segmenting.py
+++ backend/data/road_segmenting/test_segmenting.py
@@ -1,6 +1,6 @@
 import unittest
 from calculate_distance import calculate_road_length
-from road_fetcher import vegnet_to_geojson, road_network_to_file
+from road_fetcher import vegnet_to_geojson
 from road_segmenter import road_segmentor
 
 
@@ -20,17 +20,17 @@ class TestSegmenting(unittest.TestCase):
         pass
 
     def test_road_segmentor_list(self):
-        '''
+        """
         road_segmentor should return a list
         :return:
-        '''
+        """
         self.assertIsInstance(self.split_segments, list)
 
     def test_road_segmentor_list_elements(self):
-        '''
+        """
         every element in the split segments should be a dict
         :return:
-        '''
+        """
         count = 0
         for road in self.split_segments:
             if not isinstance(road, dict):
@@ -38,11 +38,11 @@ class TestSegmenting(unittest.TestCase):
         self.assertLess(count, 1)
 
     def test_split_segment_geometry_len(self):
-        '''
+        """
         Given a list of roads segments, the split segments should always have a length
         of 2 or more
         :return:
-        '''
+        """
         count = 0
         for road in self.split_segments:
             if len(road['geometry']['coordinates']) < self.min_segment_length:
@@ -51,11 +51,11 @@ class TestSegmenting(unittest.TestCase):
         self.assertLess(count, 1)
 
     def test_calculate_road_length(self):
-        '''
+        """
         The total distance of the road should be within the margin of error
         given by the variable "margin"
         :return: True or false
-        '''
+        """
         margin = 15
         road = self.road_net['features'][0]
         road_coordinates = road['geometry']['coordinates']
@@ -67,11 +67,11 @@ class TestSegmenting(unittest.TestCase):
         # This test is a little useless to be honest, we don't necessarily care
         # if a road segment goes over the limit. It does however give us an idea
         # of how accurate the segmentation is
-        '''
+        """
         Given a list of road segments, the length of the split segments should all be
         within a margin of error given by the variable "margin"
         :return:
-        '''
+        """
         margin = 50
         error = 0
         for road in self.split_segments:
@@ -79,10 +79,6 @@ class TestSegmenting(unittest.TestCase):
                 error += 1
         print(str(error) + ' veier gikk over grensen i lengde')
         self.assertLess(error, 20)
-
-    # test post to db
-
-    # test deletion from db
 
 
 if __name__ == '__main__':
