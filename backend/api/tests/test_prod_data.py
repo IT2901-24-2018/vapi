9d9d53394e7ecf0536393dfaea7435b78fedb483
Goggman <toggy@live.no>
2018-04-13 14:18:18 +0200

    Rewrote tests for map matching prod_data when using realistic data
    A bug in the tests "test_prod_data_list_authenticated" and
    "test_prod_data_list_authenticated_staff" produces unwantetd output in the console. The tests
    themselve are running fine, but the test data in the PostProductionDataTest setUp self.data are
    clutering the console. The self.data that passes the tests are printed out, which is undesirable.

--- backend/api/tests/test_prod_data.py
+++ backend/api/tests/test_prod_data.py
@@ -3,8 +3,10 @@ from django.utils import timezone
 from rest_framework import status
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
+from django.contrib.gis.geos import GEOSGeometry
 
-from api.models import ProductionData
+
+from api.models import ProductionData, DummyModel
 from api.serializers import ProductionDataSerializer
 
 
@@ -13,9 +15,17 @@ class InsertOneProductionDataTest(APITestCase):
     Test ProductionData model.
     """
     def setUp(self):
+        linestring = GEOSGeometry('LINESTRING(266711 7037272,266712 7037276,266747 7037300,266793 7037316,'
+                                  '266826 7037325,266835 7037327,266876 7037333,266916 7037334,266955 7037332,'
+                                  '267032 7037323,267127 7037314,267174 7037300,267181 7037296,267185 7037296,'
+                                  '267191 7037300)', 32633)
+        DummyModel.objects.create(the_geom=linestring)
+        d = DummyModel.objects.get()
+
         ProductionData.objects.create(
             time=timezone.now(), startlat=60.7758584, startlong=20.756444, endlat=60.45454, endlong=20.57575,
-            plow_active=True
+            plow_active=True,
+            segment=d
         )
 
     def test_prod_data(self):
@@ -47,18 +57,28 @@ class GetAllProductionDataTest(APITestCase):
         User.objects.create_user(username="normal_user", password="testpassword")
         User.objects.create_user(username="staff", password="testpassword", is_staff=True)
 
+        linestring = GEOSGeometry('LINESTRING(266711 7037272,266712 7037276,266747 7037300,266793 7037316,'
+                                  '266826 7037325,266835 7037327,266876 7037333,266916 7037334,266955 7037332,'
+                                  '267032 7037323,267127 7037314,267174 7037300,267181 7037296,267185 7037296,'
+                                  '267191 7037300)', 32633)
+        DummyModel.objects.create(the_geom=linestring)
+        d = DummyModel.objects.get()
+
+
         # Create data in db
         ProductionData.objects.create(
             time=timezone.now(), startlat=60.7758584, startlong=20.756444, endlat=60.45454, endlong=20.57575,
-            plow_active=True
+            plow_active=True,
+            segment=d
         )
         ProductionData.objects.create(
-            time=timezone.now(), startlat=60.4564577, startlong=20.465565, endlat=60.646566, endlong=20.45645,
-            plow_active=True
+            time=timezone.now(), startlat=60.4564577, startlong=20.465565, endlat=60.646566, endlong=20.4564,
+            segment=d
         )
         ProductionData.objects.create(
             time=timezone.now(), startlat=60.56345345, startlong=20.3453453, endlat=60.3453453, endlong=20.4354,
-            wet_spreader_active=True
+            wet_spreader_active=True,
+            segment=d
         )
 
     def test_get_all_prod_data_authenticated_staff(self):
@@ -111,16 +131,23 @@ class PostProductionDataTest(APITestCase):
         User.objects.create_user(username="normal_user", password="testpassword")
         User.objects.create_user(username="staff", password="testpassword", is_staff=True)
 
+        linestring = GEOSGeometry('LINESTRING(266711 7037272,266712 7037276,266747 7037300,266793 7037316,'
+                                  '266826 7037325,266835 7037327,266876 7037333,266916 7037334,266955 7037332,'
+                                  '267032 7037323,267127 7037314,267174 7037300,267181 7037296,267185 7037296,'
+                                  '267191 7037300)', 32633)
+        DummyModel.objects.create(the_geom=linestring)
+
+
         # Make the test data
         self.data = [{
-            "time": "2018-02-02T00:00:00", "startlat": 60.7758584, "startlong": 20.756444, "endlat": 60.45454,
-            "endlong": 20.57575, "plow_active": True
+            "time": "2018-02-02T00:00:00", "startlat": 63.387075002372903, "startlong": 10.3277250005425,
+            "endlat": 60.45454, "endlong": 20.57575, "plow_active": True
         }, {
-            "time": "2018-02-02T00:01:00", "startlat": 60.4564577, "startlong": 20.465565, "endlat": 60.646566,
-            "endlong": 20.45645, "plow_active": True
+            "time": "2018-02-02T00:01:00", "startlat": 63.387691997704202, "startlong": 10.3290819995141,
+            "endlat": 60.646566, "endlong": 20.45645, "plow_active": True
         }, {
-            "time": "2018-02-02T00:02:00", "startlat": 60.56345345, "startlong": 20.3453453, "endlat": 60.3453453,
-            "endlong": 20.4354, "wet_spreader_active": True
+            "time": "2018-02-02T00:02:00", "startlat": 63.387642998353499, "startlong": 10.3282330021124,
+            "endlat": 60.3453453, "endlong": 20.4354, "wet_spreader_active": True
         }]
 
     def test_post_prod_data_list_authenticated(self):
@@ -135,9 +162,9 @@ class PostProductionDataTest(APITestCase):
         response = self.client.post(url, self.data, format='json')
 
         # Check the status code, then check that the number of objects in the database matches the number
-        # of objects in the POST request
+        # of objects that are within range in the POST request
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(ProductionData.objects.count(), len(self.data))
+        self.assertEqual(ProductionData.objects.count(), 1)
 
     def test_post_prod_data_list_authenticated_staff(self):
         """
@@ -150,10 +177,11 @@ class PostProductionDataTest(APITestCase):
         url = reverse('productiondata-list')
         response = self.client.post(url, self.data, format='json')
 
+
         # Check the status code, then check that the number of objects in the database matches the number
-        # of objects in the POST request
+        # of objects that are within range in the POST request
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(ProductionData.objects.count(), len(self.data))
+        self.assertEqual(ProductionData.objects.count(), 1)
 
     def test_post_prod_data_list_unauthenticated(self):
         """
