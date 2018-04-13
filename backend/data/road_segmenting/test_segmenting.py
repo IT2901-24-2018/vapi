f0de2d2b06f08ba92e99360024645c0c5c241152
flamberhamber <teipfreak@hotmail.com>
2018-04-13 16:10:32 +0200

    rewrite road length test, redefine error messages

--- backend/data/road_segmenting/test_segmenting.py
+++ backend/data/road_segmenting/test_segmenting.py
@@ -2,7 +2,7 @@ import unittest
 
 from calculate_distance import calculate_road_length
 from road_fetcher import vegnet_to_geojson
-from road_segmenter import segment_network
+from road_segmenter import segment_network, split_segment
 
 
 class TestSegmenting(unittest.TestCase):
@@ -16,7 +16,7 @@ class TestSegmenting(unittest.TestCase):
         network = vegnet_to_geojson(self.kommune, self.vegref)
         self.count, self.road_net = network[0], network[1]
         self.split_segments = segment_network(self.road_net, self.count,
-                                             self.max_segment_length, self.min_segment_length)
+                                              self.max_segment_length, self.min_segment_length)
 
     def setUp(self):
         pass
@@ -26,7 +26,7 @@ class TestSegmenting(unittest.TestCase):
         road_segmenter should return a list
         :return:
         """
-        self.assertIsInstance(self.split_segments, list)
+        self.assertIsInstance(self.split_segments, list, "The road segmenter did not return a list")
 
     def test_road_segmenter_list_elements(self):
         """
@@ -34,10 +34,13 @@ class TestSegmenting(unittest.TestCase):
         :return:
         """
         count = 0
+        error_message = "Not all elements in the split list are of type dict: \n"
         for road in self.split_segments:
             if not isinstance(road, dict):
                 count += 1
-        self.assertLess(count, 1)
+                error_message += "Index: " + str(self.split_segments.index(road)) + ", typeof: " + \
+                                 str(type(road)) + "\n"
+        self.assertLess(count, 1, error_message)
 
     def test_split_segment_geometry_len(self):
         """
@@ -46,11 +49,13 @@ class TestSegmenting(unittest.TestCase):
         :return:
         """
         count = 0
+        error_message = "These segments have less than " + str(self.min_segment_length) + " GPS points \n"
         for road in self.split_segments:
             if len(road['geometry']['coordinates']) < self.min_segment_length:
                 count += 1
-                print(road['properties']['veglenkeid'], road['geometry']['coordinates'])
-        self.assertLess(count, 1)
+                error_message += "Veglenkeid: " + str(road['properties']['veglenkeid']) + ", coordinates: " + \
+                                 str(road['geometry']['coordinates']) + "\n"
+        self.assertLess(count, 1, error_message)
 
     def test_calculate_road_length(self):
         """
@@ -58,8 +63,26 @@ class TestSegmenting(unittest.TestCase):
         a margin given by the variable "margin"
         :return: True or false
         """
-        pass
+        margin = 8
+        errors = 0
+        error_message = "Issues are with these links: \n"
+        for key, values in self.road_net.items():
+            if key != 'crs':
+                for i in range(0, self.count):
+                    road = self.road_net['features'][i]
+                    length_actual = road['properties']['strekningslengde']
+                    length_original = calculate_road_length(road['geometry']['coordinates'], 1000, False)[1]
+                    road_segmented = split_segment(road, self.max_segment_length, [], self.min_segment_length)
 
+                    length_new = 0
+                    for road in road_segmented:
+                        length_new += calculate_road_length(road['geometry']['coordinates'], 1000, False)[1]
+                    if abs(length_actual - length_new) > margin:
+                        errors += 1
+                        error_message += "Veglenkeid: " + str(road['properties']['veglenkeid']) + \
+                                         ", actual length: " + str(length_actual) + ", original: " + \
+                                         str(length_original) + ", new:" + str(length_new) + "\n"
+        self.assertLess(errors, 1, error_message)
 
     def test_split_segment_road_length(self):
         # This test is a little useless to be honest, we don't necessarily care
@@ -71,12 +94,12 @@ class TestSegmenting(unittest.TestCase):
         :return:
         """
         margin = 50
-        error = 0
+        errors = 0
         for road in self.split_segments:
             if (road['properties']['strekningslengde'] - self.max_segment_length) > margin:
-                error += 1
-        print(str(error) + ' veier gikk over grensen i lengde')
-        self.assertLess(error, 20)
+                errors += 1
+        error_message = (str(errors) + ' roads exceeded the road length limit')
+        self.assertLess(errors, 20, error_message)
 
 
 if __name__ == '__main__':
