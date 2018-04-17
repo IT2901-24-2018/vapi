e514b86b90564cf8fa4b214ebdee69e71f8c1113
sardred <andelar96@gmail.com>
2018-04-17 21:39:29 +0200

    Left out a crucial part to the Docker commit, namely the database

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -35,6 +35,20 @@ Debug = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': '5432'
+    }
+}
+
+
 # Application definition
 
 INSTALLED_APPS = [
