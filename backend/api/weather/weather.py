fb96321e69407ec0f81cc94ff65b0f7679db9412
pontius1007 <embrikeinang@msn.com>
2018-04-25 16:43:49 +0200

    Create function

---
+++ backend/api/weather/weather.py
@@ -0,0 +1,4 @@
+def map_weather_to_segment(weather_data):
+    # Todo Map the weather to every segment.
+    # Check if exisiting weather exists and add total value if needed
+    return weather_data
