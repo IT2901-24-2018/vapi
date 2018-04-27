5296366883e5d594db969cde4edfce7f706e8cdd
pontius1007 <embrikeinang@msn.com>
2018-04-27 17:16:37 +0200

    Add description

--- apps/api/models.py
+++ apps/api/models.py
@@ -15,29 +15,36 @@ class BaseModel(models.Model):
 
 
 class RoadSegment(BaseModel):
-    county = models.IntegerField()
-    href = models.CharField(max_length=150)
-    category = models.CharField(max_length=4)
-    municipality = models.IntegerField()
-    startdate = models.DateField()
-    region = models.IntegerField()
-    status = models.CharField(max_length=4)
-    stretchdistance = models.IntegerField()
-    typeofroad = models.CharField(max_length=100)
-    roadsectionid = models.IntegerField()
-    vrefshortform = models.CharField(max_length=255)
-    the_geom = models.LineStringField()
+    county = models.IntegerField(help_text="County identifier. Example: '50'")
+    href = models.CharField(max_length=150, help_text="Link to NVDB for this unique segment")
+    category = models.CharField(max_length=4, help_text="Road segment category. Example: 'K'")
+    municipality = models.IntegerField(help_text="Municipality number for that county. "
+                                       "Example: '01' for Trondheim")
+    startdate = models.DateField(help_text="Start date for the road segment. Example: '2018-04-20'")
+    region = models.IntegerField(help_text="Region number. Example: '4'")
+    status = models.CharField(max_length=4, help_text="Road status. Example: 'G'")
+    stretchdistance = models.IntegerField(help_text="Length of the road segment. Example '31'")
+    typeofroad = models.CharField(max_length=100, help_text="A description of the road type. "
+                                  "Example: 'gangOgSykkelvei'")
+    roadsectionid = models.IntegerField(help_text="Unique identifier for the road segment. "
+                                        "Example: '171712'")
+    vrefshortform = models.CharField(max_length=255, help_text="A combination of multiple fields. "
+                                     "Example: '5001 Kg97587 hp1 m349-380'")
+    the_geom = models.LineStringField(help_text="Linestring according to ISO 19162:2015. "
+                                      "Example: 'SRID=4326;LINESTRING (10.37634290477487 63.3478716972899, "
+                                      "10.37656821856063 63.34786722088941)'")
 
 
 class ProductionData(BaseModel):
-    time = models.DateTimeField()
-    startlat = models.FloatField()
-    startlong = models.FloatField()
-    endlat = models.FloatField()
-    endlong = models.FloatField()
-    dry_spreader_active = models.NullBooleanField()
-    plow_active = models.NullBooleanField()
-    wet_spreader_active = models.NullBooleanField()
-    brush_active = models.NullBooleanField()
-    material_type_code = models.IntegerField(null=True)
-    segment = models.ForeignKey(RoadSegment, on_delete=models.CASCADE)
+    time = models.DateTimeField(help_text="When the production data was generated. Example: '2016-11-04T08:45:15Z'")
+    startlat = models.FloatField(help_text="Start latitude. Example: '63.3870750023729'")
+    startlong = models.FloatField(help_text="Start longitute. Example: '10.3277250005425'")
+    endlat = models.FloatField(help_text="End latitude: Example: ' 63.3874419990294'")
+    endlong = models.FloatField(help_text="End longitude: Example: '10.3290930003037'")
+    dry_spreader_active = models.NullBooleanField(help_text="Dry spreader active boolean. Optional.")
+    plow_active = models.NullBooleanField(help_text="Plow boolean. Optional.")
+    wet_spreader_active = models.NullBooleanField(help_text="Wet spreader boolean. Optional.")
+    brush_active = models.NullBooleanField(help_text="Brush boolean. Optional.")
+    material_type_code = models.IntegerField(null=True, help_text="Material type boolean. Optional")
+    segment = models.ForeignKey(RoadSegment, on_delete=models.CASCADE, help_text="Segment ID to mapped segment. "
+                                "Will be autocompleted by the code. Enter dummy value.")
