e1cb45c2f033719033775a590a4f55e597e7a91d
pontius1007 <embrikeinang@msn.com>
2018-05-02 13:20:36 +0200

    Now properly gathers and loop every element

--- apps/api/weather/weather.py
+++ apps/api/weather/weather.py
@@ -4,12 +4,15 @@ from api.models import RoadSegment
 def map_weather_to_segment(weather_data):
     # Todo Map the weather to every segment.
     # Check if exisiting weather exists and add total value if needed
-    segments = get_segments()
-
-
-    return
+    for weather in weather_data:
+        mun_segments = get_segments(weather['county_and_municipality_id'])
+        for road in mun_segments:
+            #Here every segment will be listed
+            return road
+    return weather_data
 
 
 def get_segments(municipality):
-    matched_segments = RoadSegment.objects.filter(municipality=municipality)
+    queryset = RoadSegment.objects.filter(municipality=municipality).values()
+    matched_segments = list(queryset)
     return matched_segments
