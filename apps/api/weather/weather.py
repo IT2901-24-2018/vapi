62152097473ad9857c87818d00733e77c12e796f
Sebastian Olafsson <msemse.sebastian@gmail.com>
2018-05-02 12:14:18 +0200

    added querying weather by munincipality

--- apps/api/weather/weather.py
+++ apps/api/weather/weather.py
@@ -1,4 +1,27 @@
+from django.db import connection
+
+
 def map_weather_to_segment(weather_data):
     # Todo Map the weather to every segment.
     # Check if exisiting weather exists and add total value if needed
-    return weather_data
+    segments = get_segments(5001)
+
+
+    return segments
+
+
+def get_segments(municipality):
+    with connection.cursor() as cursor:
+        stmt = """
+        SELECT * FROM api_roadsegment
+        WHERE api_roadsegment.municipality=municipality
+        """
+        cursor.execute(stmt, municipality)
+        data = cursor.fetchall()
+    if data:
+        return data
+    else:
+        return None
+
+
+map_weather_to_segment(3)
