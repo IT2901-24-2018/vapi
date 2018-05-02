9160b5e4a9e0a233fab620d25efa0f52310f4646
pontius1007 <embrikeinang@msn.com>
2018-05-02 14:22:23 +0200

    Small fix

--- apps/api/weather/weather.py
+++ apps/api/weather/weather.py
@@ -7,12 +7,11 @@ def map_weather_to_segment(weather_data):
     for weather in weather_data:
         mun_segments = get_segments(weather['county_and_municipality_id'])
         for road in mun_segments:
-            #Here every segment will be listed
-            return road
+            print("Do something")
     return weather_data
 
 
 def get_segments(municipality):
-    queryset = RoadSegment.objects.filter(municipality=municipality).values()
+    queryset = RoadSegment.objects.filter(municipality=municipality).values('id')
     matched_segments = list(queryset)
     return matched_segments
