2c13de9ac651637cbf05a2a0532014c46fea43ce
pontius1007 <embrikeinang@msn.com>
2018-05-04 16:04:11 +0200

    Now updates end time

--- apps/api/weather/weather.py
+++ apps/api/weather/weather.py
@@ -2,8 +2,8 @@ from api.models import RoadSegment, WeatherData
 from django.db.models import F
 from copy import deepcopy
 
-
 def map_weather_to_segment(weather_data):
+    # Handle if the input has an old time associated with it
     number_of_updated_weather = 0
     mapped_weather = []
 
@@ -28,13 +28,15 @@ def update_weather_data(inserted_weather, segment_id):
     weather = WeatherData.objects.get(segment=segment_id)
     weather.value = F('value') + inserted_weather['value']
     weather.degrees = inserted_weather['degrees']
-    weather.save(update_fields=['value', 'degrees'])
+    weather.end_time_period = inserted_weather['end_time_period']
+    weather.save(update_fields=['value', 'degrees', 'end_time_period'])
 
 
 def get_segments(municipality):
     queryset = RoadSegment.objects.filter(municipality=municipality).values('id')
     matched_segments = list(queryset)
     return matched_segments
+
 
 def get_weather_for_mun(municipality):
     queryset = WeatherData.objects.filter(county_and_municipality_id=municipality).values()
