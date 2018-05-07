a30c0b28839046f55f8feb7ed1946c21e7e95468
pontius1007 <embrikeinang@msn.com>
2018-05-07 12:48:02 +0200

    Call weather to check for overlapping weather and prod data

--- apps/api/views.py
+++ apps/api/views.py
@@ -113,6 +113,9 @@ class ProductionDataViewSet(viewsets.ModelViewSet):
         # Handle overlap with old prod-data
         mapped_data = mapper.handle_prod_data_overlap(mapped_data)
 
+        # Handle weather data when adding new production data
+        weather.handle_prod_weather_overlap(mapped_data)
+
         # Instantiate the serializer
         serializer = self.get_serializer(data=mapped_data, many=True)
 
